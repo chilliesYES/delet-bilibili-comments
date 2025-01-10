180fd5c6a4326e3fbc07a5046453874d66764250
chilliesYES <132345386+chilliesYES@users.noreply.github.com>
2025-01-10 16:57:20 +0800

    Update main.py
    add mkdir

--- main.py
+++ main.py
@@ -32,15 +32,25 @@ aicu截的json放jsont
 json_t是放被回复数据的
 '''
 
-ask0 = input('1.处理从aicu获取的json上传数据库并执行删除\n2.获取被回复的消息上传并执行删除')
-if ask0 == '1':
-    mode = 1
-    pass
-if ask0 == '2':
-    mode = 2
-    getnew.get_reply()
-    json_to_sql.main(connect, mode)
-    go_del.main(connect)
+while True:
+    ask0 = input('1.处理从aicu获取的json上传数据库并执行删除\n2.获取被回复的消息上传并执行删除\n3.创建必要文件夹\n4.退出')
+    if ask0 == '1':
+        mode = 1
+        pass
+    elif ask0 == '2':
+        mode = 2
+        getnew.get_reply()
+        json_to_sql.main(connect, mode)
+        go_del.main(connect)
+    elif ask0 == '3':
+        rr = Path('json_folder/json_r/');rr.mkdir(exist_ok=True)
+        tt = Path('json_folder/jsont/');tt.mkdir(exist_ok=True)
+    elif ask0 == '4':
+        sys.exit()
+
+
+
+
 
 
 
